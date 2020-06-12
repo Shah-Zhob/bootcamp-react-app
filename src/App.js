b5ef8a9e00b9aa7637ed87453c8a1129d13318a5
Shah-Zhob <66476271+Shah-Zhob@users.noreply.github.com>
2020-06-12 06:46:14 -0400

    change in app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <h1>hello world from all of us</h1>       
+       <h1>hello world from shahzhob</h1>       
     </div>
   );
 }
